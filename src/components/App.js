de639b120196fbd27064638d7193fbdc905db63d
Christy Welsh <christyannwelsh@gmail.com>
2018-08-07 10:14:14 -0700

    Phase Two
    click button to load films

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,31 @@ import Films from './Films'
 class App extends Component {
   constructor() {
     super();
-    this.state = { films: null };
-  }; 
+    this.state = {
+      button: false,
+      films: null
+    };
+    this.handleClick = this.handleClick.bind(this)
+  };
 
-  componentDidMount() {
-    fetch("https://ghibliapi.herokuapp.com/films")
-      .then(res => res.json())    
-      .then(data => this.setState({ films: data }))
-      .catch(e => console.log(e))      
+  handleClick() {
+    this.setState(prevState => ({
+      button: !prevState.button
+    }));
   }
 
   render() {
+
+    if (this.state.button) {
+      fetch("https://ghibliapi.herokuapp.com/films")
+        .then(res => res.json())
+        .then(data => this.setState({ films: data }))
+        .catch(e => console.log(e))
+    }
+
     return (
       <div>
-        <Top />
+        <Top handleClick={this.handleClick} />
         <Films filmsArray={this.state.films} />
         <Cards />
       </div>
--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Top = () => {
+const Top = (props) => {
+    
     return (
         <div className='top'>
             <img src='https://upload.wikimedia.org/wikipedia/en/thumb/c/ca/Studio_Ghibli_logo.svg/1200px-Studio_Ghibli_logo.svg.png' height="150px" alt="Studio Ghibli Films"></img>
-            <button>Load Films</button>
+            <button onClick={props.handleClick}>Load Films</button>
         </div>
     )
 }
